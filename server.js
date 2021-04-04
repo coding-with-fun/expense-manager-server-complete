e935f277b4db1ca5b3957d66ba05da0a617240e9
Coderc <harsh@coderc.in>
2021-04-04 20:27:38 +0530

    Connected to MongoDB and added package dependencies

--- server.js
+++ server.js
@@ -1,15 +1,50 @@
+/**
+ * @author Coderc
+ * @description Entry file for server.
+ */
+
+/**
+ * @description Package dependencies.
+ */
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const express = require("express");
 require("colors");
 require("dotenv").config();
 
-const app = express();
+/**
+ *  @description Internal dependencies.
+ */
+const connectDB = require("./config/db");
 
+/**
+ *  @description Defining variables.
+ */
+const app = express();
 const PORT = process.env.PORT;
 
+/**
+ * @description Configuring middleware.
+ */
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+/**
+ *  @description Establishing Server Connection.
+ */
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`.red);
+    console.log(`Server is running on port ${PORT}...`.magenta);
 });
 
+/**
+ *  @description Connecting to MongoDB.
+ */
+connectDB();
+
+/**
+ *  @description Defining Routes.
+ */
 app.use("/", (req, res) => {
     res.json({
         message: "Hello Sir...",
